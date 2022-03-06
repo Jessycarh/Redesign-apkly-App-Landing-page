651174c60eca5168d5e7c06cd7ac2806214ab99a
Jessycarh <jessyjay77@gmail.com>
2022-03-06 16:38:08 +0100

    Div 2 Apkly redesign

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+import './App.css';
+import Div2  from "./Components/Div2/Div2.js";
+
+
+function App () { 
+  return( 
+    <div>
+<>
+  <Div2/>
+</>
+    
+  </div>
+  )
+ 
+}
+
+export default App
